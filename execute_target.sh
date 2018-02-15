75b6354c2cda0a65aae0937d6c2b0a3508e2a2f5
BugUser0815 <ga67wel@mytum.de>
2018-02-15 15:46:04 +0100

    Move run files to evaluation/tool

--- execute_target.sh
+++
@@ -1,6 +0,0 @@
-sudo make -C build/genode-$1/ run/$2 &
-sleep 10
-sudo rm -f /var/lib/tftpboot/image.elf
-sudo cp build/genode-$1/var/run/$2/image.elf /var/lib/tftpboot/
-sudo ./kill_minicom &
-sudo minicom -D /dev/ttyUSB0 -C log/$1/bare_and_tasks/$2.txt
--- os_and_components.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GENODE_TARGET="focnados_pbxa9"
-make -C build/genode-$GENODE_TARGET/ run/controller_on_core_one >> log/$GENODE_TARGET/os_and_components/controller_on_core_one_fp.txt 2>&1
-make -C build/genode-$GENODE_TARGET/ run/dom0_on_core_one >> log/$GENODE_TARGET/os_and_components/dom0_on_core_one_fp.txt 2>&1
-make -C build/genode-$GENODE_TARGET/ run/monitor_on_core_one >> log/$GENODE_TARGET/os_and_components/monitor_on_core_one_fp.txt 2>&1
-make -C build/genode-$GENODE_TARGET/ run/parser_on_core_one >> log/$GENODE_TARGET/os_and_components/parser_on_core_one_fp.txt 2>&1
-make -C build/genode-$GENODE_TARGET/ run/sync_on_core_one >> log/$GENODE_TARGET/os_and_components/sync_on_core_one_fp.txt 2>&1
-make -C build/genode-$GENODE_TARGET/ run/taskloader_on_core_one >> log/$GENODE_TARGET/os_and_components/taskloader_on_core_one_fp.txt 2>&1
-make -C build/genode-$GENODE_TARGET/ run/utilization_on_core_one >> log/$GENODE_TARGET/os_and_components/utilization_on_core_one_fp.txt 2>&1
